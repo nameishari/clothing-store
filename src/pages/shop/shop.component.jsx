7fb84553fb4728d6c699d0dae2fc957a33da4035
nameishari <hariharakumar88@gmail.com>
2021-02-07 17:23:27 +0100

    use redux-thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import CollectionsOverview from "../../componets/collection-overview/collection-overview.componet";
 import CollectionPage from "../collection/collection.componet";
 import { Route } from "react-router-dom";
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from "../../firebase/firebase.utils";
-import { updateCollections } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 import { connect } from "react-redux";
 import WithSpinner from "../../componets/with-spinner/with-spinner.componet";
+import { createStructuredSelector } from "reselect";
+import {
+  selectIsCollectionsLoaded,
+  selectIsCollectionFetching,
+} from "../../redux/shop/shop.selectors";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
@@ -16,34 +17,38 @@ class ShopPage extends React.Component {
   state = {
     loading: true,
   };
-  
+
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection("collections");
-    collectionRef.get().then(async (snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
   }
 
   render() {
     // match comes from <Route></Route> in app.js for ShopePage
-    const { match } = this.props;
-    const { loading } = this.state;
+    const {
+      match,
+      isCollectionsLoaded,
+      isCollectionFetching,
+    } = this.props;
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={(props) => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
         <Route
           path={`${match.path}/:collectionId`}
           render={(props) => (
-            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+            <CollectionsPageWithSpinner
+              isLoading={!isCollectionsLoaded}
+              {...props}
+            />
           )}
         />
       </div>
@@ -52,8 +57,12 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) =>
-    dispatch(updateCollections(collectionsMap)),
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapPropsFromState = createStructuredSelector({
+  isCollectionsLoaded: selectIsCollectionsLoaded,
+  isCollectionFetching: selectIsCollectionFetching,
+});
+
+export default connect(mapPropsFromState, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,4 @@
 import { createSelector } from "reselect";
-import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -12,9 +11,18 @@ export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     (collections) => collections ? Object.keys(collections).map(key => collections[key]) : []
 )
-export const selectCollection = memoize((collectionUrlParam) =>
+export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    (collections) => (collections ? collections[collectionUrlParam] : null)
-  )
+    collections => (collections ? collections[collectionUrlParam] : null)
+  );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
 );
